f07b5b895349ec9cad7e1f0615bcfffde321bd9c
bobbyhadz <opmftw@gmail.com>
2024-03-02 13:45:52 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const myDropdown = document.getElementById(
+  'dropdownMenuButton1',
+);
+
+myDropdown.addEventListener('show.bs.dropdown', function () {
+  console.log('Dropdown shown');
+});
